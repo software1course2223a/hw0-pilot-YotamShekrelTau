79ea93e2aeb68ce4d3c483b0c9297c0c51bc79a2
YotamShekrelTau <yotamshekrel@mail.tau.ac.il>
2022-10-28 15:16:57 +0300

    first try

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("");
-	}
-}
+	System.out.println("hey there!");
+	System.out.println("if you can read it, it means that everything is working");
+}}
